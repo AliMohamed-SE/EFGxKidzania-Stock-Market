f88a18b6d811e398d827f61ca73c017f643106e2
AliSaadOthman <96048321+AliSaadOthman@users.noreply.github.com>
2025-02-08 20:58:01 +0200

    ui revamp matching local

--- stock_market_UI/src/components/SellStockWindow.jsx
+++ stock_market_UI/src/components/SellStockWindow.jsx
@@ -114,7 +114,7 @@ const SellStockWindow = ({ company, withdrawHandle }) => {
             <div className="flex flex-row justify-between items-center">
               {" "}
               <input
-                value={(quantity * company.price).toLocaleString()}
+                value={quantity * company.price}
                 className="bg-white-100 text-lg w-[50%]"
                 disabled
               />
